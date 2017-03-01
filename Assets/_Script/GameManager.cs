01589220241051eee266423ddc21c66ec8bb56ab
ETU\n.pankowiak <n.pankowiak@rubika-edu.com>
2017-03-01 22:52:59 +0100

    Advanced prototype + New Settings Menu + New Inputs method

--- Assets/_Script/GameManager.cs
+++ Assets/_Script/GameManager.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance;
     public bool loaded;
     public bool gamestarted;
+    public bool hasGameLaunched;
     public float chunkSize;
+
+    PlayerController playerController;
 
     void Awake()
     {
@@ -20,16 +24,31 @@ public class GameManager : MonoBehaviour {
             instance.loaded = true;
             Destroy(gameObject);
         }
+
         gamestarted = false;
+        hasGameLaunched = false;
 
         Screen.orientation = ScreenOrientation.Portrait;
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
 
         //Set camera size depending on aspect ratio
         Camera.main.orthographicSize = ((Screen.height * chunkSize) / Screen.width) / 2;
+
     }
 
     // Update is called once per frame
     void Update () {
 		
 	}
+
+    public void LaunchGame()
+    {
+        gamestarted = true;
+        playerController.gameObject.transform.DOMoveY(playerController.targetPosition.y, 1.75f).SetEase(Ease.InOutBack).OnComplete(GameLaunched);
+    }
+
+    public void GameLaunched()
+    {
+        hasGameLaunched = true;
+    }
 }
--- Assets/_Script/UIManager.cs
+++ Assets/_Script/UIManager.cs
@@ -1,33 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
 using DG.Tweening;
 
 public class UIManager : MonoBehaviour {
 
+    [Header("MAIN MENU")]
+    public Canvas mainMenuCanvas;
     public GameObject title;
     public GameObject startText;
     public GameObject achievements;
     public GameObject settings;
     public GameObject leaderboards;
+    public GameObject start;
 
+    [Header("SETTINGS")]
     public Canvas settingsCanvas;
+    public GameObject On;
+    public GameObject Off;
+    public GameObject Normal;
+    public GameObject High;
+    public GameObject Right;
+    public GameObject Left;
+    public GameObject Yes;
+    public GameObject No;
+    public Color unselected;
+
+    [Header("PLAYERUI")]
+    public Canvas playerUI;
+
+    bool isSound;
+    bool isNormal;
+    bool isRight;
+    bool isRate;
 
     GameManager gm;
     bool hasleft;
+    bool clikedButton;
 
 	// Use this for initialization
 	void Start () {
-        RotateRight();
-        ScaleUp();
+        TitleFeedback();
+        StartFeedback();
         gm = GameManager.instance;
         hasleft = false;
+
+        isSound = true;
+        isNormal = true;
+        isRight = true;
+        isRate = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gm.gamestarted == true && hasleft == false)
+        if (gm.gamestarted && !hasleft)
         {
             LeaveUp();
             LeaveDown();
@@ -38,45 +67,107 @@ public class UIManager : MonoBehaviour {
 
     public void ShowAchievements()
     {
+        if (!SplashScreen.isFinished) return;
         settingsCanvas.gameObject.SetActive(true);
+        achievements.transform.DOPunchScale(new Vector3(0.2f, 0.2f, 1), 1f, 4, 1);
     }
 
     public void ShowSettings()
     {
+        if (!SplashScreen.isFinished) return;
         settingsCanvas.gameObject.SetActive(true);
+        settings.transform.DOPunchScale(new Vector3(0.2f, 0.2f, 1), 1f, 4, 1);
     }
 
     public void ShowLeaderboards()
     {
+        if (!SplashScreen.isFinished) return;
         settingsCanvas.gameObject.SetActive(true);
+        leaderboards.transform.DOPunchScale(new Vector3(0.2f, 0.2f, 1), 1f, 4, 1);
+    }
+
+    public void HideSettings()
+    {
+        settingsCanvas.gameObject.SetActive(false);
     }
 
     public void GameStart()
     {
+        if (!SplashScreen.isFinished) return;
+        gm.LaunchGame();
         achievements.SetActive(false);
         settings.SetActive(false);
         leaderboards.SetActive(false);
+        start.SetActive(false);
     }
+
+    public void ToggleSound(bool isOn)
+    {
+        if (isSound && !isOn)
+        {
+            isSound = false;
+            On.GetComponent<Text>().color = unselected;
+            Off.transform.DOScale(0.8f, 0.1f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+            Off.GetComponent<Text>().color = Color.white;
+            AudioListener.volume = 0;
+        }else if(!isSound && isOn)
+        {
+            isSound = true;
+            On.GetComponent<Text>().color = Color.white;
+            On.transform.DOScale(0.8f, 0.1f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+            Off.GetComponent<Text>().color = unselected;
+            AudioListener.volume = 1;
+        }
+    }
+
+    public void ToggleQuality(bool isGood)
+    {
+        if (isNormal && !isGood)
+        {
+            isNormal = false;
+            Normal.GetComponent<Text>().color = unselected;
+            High.transform.DOScale(0.8f, 0.1f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+            High.GetComponent<Text>().color = Color.white;
+            QualitySettings.SetQualityLevel(5);
+        }
+        else if (!isNormal && isGood)
+        {
+            isNormal = true;
+            Normal.GetComponent<Text>().color = Color.white;
+            Normal.transform.DOScale(0.8f, 0.1f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+            High.GetComponent<Text>().color = unselected;
+            QualitySettings.SetQualityLevel(2);
+        }
+    }
+
+    public void ToggleHand(bool isRightHanded)
+    {
+        if (isRight && !isRightHanded)
+        {
+            isRight = false;
+            Right.GetComponent<Text>().color = unselected;
+            Left.transform.DOScale(0.8f, 0.1f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+            Left.GetComponent<Text>().color = Color.white;
+        }
+        else if (!isRight && isRightHanded)
+        {
+            isRight = true;
+            Right.GetComponent<Text>().color = Color.white;
+            Right.transform.DOScale(0.8f, 0.1f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+            Left.GetComponent<Text>().color = unselected;
+        }
+    }
+
 
     // Visual Feedbacks
-    void RotateRight()
+    void TitleFeedback()
     {
-        title.transform.DORotate(new Vector3(0, 0, -1f), 2f).OnComplete(RotateLeft);
+        title.transform.DORotate(new Vector3(0, 0, -2f), 1.5f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
     }
 
-    void RotateLeft()
+    void StartFeedback()
     {
-        title.transform.DORotate(new Vector3(0, 0, 1f), 2f).OnComplete(RotateRight);
-    }
-
-    void ScaleUp()
-    {
-        startText.transform.DOScale(0.4f, 1f).OnComplete(ScaleDown).SetEase(Ease.OutQuart);
-    }
-
-    void ScaleDown()
-    {
-        startText.transform.DOScale(0.5f, 1f).OnComplete(ScaleUp).SetEase(Ease.OutQuart);
+        startText.transform.DOScale(0.4f, 0.2f).SetEase(Ease.Linear).SetLoops(-1,LoopType.Yoyo);
     }
 
     void LeaveUp()
