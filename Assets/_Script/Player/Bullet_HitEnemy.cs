36a82a657be0eabb240e95d11a9676f42555edc4
kuarisu <kuarisuran@gmail.com>
2017-03-01 12:12:07 +0100

    Enemy life is working. Added a script for the bullet to detect the enemy and call the function.
    Don't forget to add enemy tag to enemy.

---
+++ Assets/_Script/Player/Bullet_HitEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_HitEnemy : MonoBehaviour {
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "enemy" && col.gameObject.GetComponent<Enemy_Lives>() != null)
+        {
+            col.gameObject.GetComponent<Enemy_Lives>().Hited();
+        }
+    }
+}
