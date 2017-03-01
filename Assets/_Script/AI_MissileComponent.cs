eaa5943e09df8db75632a689bc92d01d2566382d
ETU\s.debank <s.debank@rubika-edu.com>
2017-03-01 14:05:00 +0100

    Component Turret and Missile
    Component Turret and Missile, inside Gameobject but can be used on new
    enemy

---
+++ Assets/_Script/AI_MissileComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_MissileComponent : MonoBehaviour {
+
+	public float speedAIMissile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (transform.up * speedAIMissile, Space.World);
+		//transform.position = new Vector3 (transform.position.x, transform.position.y, 0.0f);
+	}
+
+	void OnCollisionEnter2D (Collision2D other){
+		if (other.gameObject.name == "Player"){
+			Debug.Log ("cc");
+			Destroy (this.gameObject);
+		}
+
+		Debug.Log ("slt");
+	}
+}
