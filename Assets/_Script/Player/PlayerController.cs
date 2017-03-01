18edffab6551235095a91cd436b768770c1b1a0d
ETU\n.pankowiak <n.pankowiak@rubika-edu.com>
2017-03-01 15:46:49 +0100

    New prototype

--- Assets/_Script/Player/PlayerController.cs
+++ Assets/_Script/Player/PlayerController.cs
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
     public float startOffset;
     public float maxOffsetY;
     public float distanceOffsetY;
+    public float maxOffsetX;
     public float distanceOffsetX;
 
     [Header("GAMEPLAY")]
@@ -27,9 +28,16 @@ public class PlayerController : MonoBehaviour
     public int startpunch;
     [Tooltip("Force applied to player when he fires bullets")]
     public int firepunch;
+    [Tooltip("Side force applied to player when he fires bullets")]
+    public int sidepunch;
+    [Tooltip("Max Side force that can be applied to player when he fires bullets")]
+    public float maxsidepunch;
     [Tooltip("Force applied to player when he falls back")]
-    public int backforce;
-    public int bulletspeed;
+    public int backForceY;
+    [Tooltip("Force applied to player to make it back to the vertical center")]
+    public int backForceX;
+    public int bulletSpeed;
+    public int bulletNumber;
 
     [Header("ROTATION")]
     [Tooltip("Rotation Force applied to player at the beginning")]
@@ -44,6 +52,7 @@ public class PlayerController : MonoBehaviour
     [Header("SCREENSHAKE")]
     public float strenght;
     public float duration;
+    public int vibrato;
 
     // Use this for initialization
     void Awake()
@@ -59,24 +68,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gm.gamestarted == false)
-            this.transform.Rotate(Vector3.forward, 50 * Time.deltaTime);
+        //if (gm.gamestarted == false)
+            //this.transform.Rotate(Vector3.forward, 50 * Time.deltaTime);
 
         if(SplashScreen.isFinished == true)
             Controls();
 
-        distanceOffsetY = transform.position.y - targetPosition.y;
-        float distanceToMax = maxOffsetY - distanceOffsetY;
-
-        if(Mathf.Abs(distanceOffsetY) > 0.1f)
-        {
-            Vector2 vectorToNormalize = new Vector2(0f, targetPosition.y - transform.position.y);
-            vectorToNormalize.Normalize();
-            rb.AddForce(vectorToNormalize * backforce * Mathf.Abs(distanceOffsetY / maxOffsetY), ForceMode2D.Force);
-            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, 1.5f, 7.5f), transform.position.z);
-        }
-
-        distanceOffsetX = transform.position.x - targetPosition.x;
+        BackForceY();
+        BackForceX();
     }
 
     // Controls for the player
@@ -85,7 +84,7 @@ public class PlayerController : MonoBehaviour
 
         //INPUTS FOR EDITOR AND STANDALONE
 #if UNITY_EDITOR || UNITY_STANDALONE
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && Camera.main.ScreenToWorldPoint(Input.mousePosition).y < transform.position.y /*&& bulletNumber > 0*/)
         {
             Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position;
             direction.Normalize();
@@ -98,14 +97,14 @@ public class PlayerController : MonoBehaviour
             }
             else
             {
-                rb.AddForce(-direction * firepunch, ForceMode2D.Impulse);
+                rb.AddForce(new Vector2(Mathf.Clamp(-direction.x * sidepunch, -maxsidepunch, maxsidepunch), -direction.y) * firepunch, ForceMode2D.Impulse);
                 bulletFired = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
 
                 // Rotate the bullet towards the input
                 float rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
                 bulletFired.transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
 
-                bulletFired.GetComponent<Rigidbody2D>().velocity = direction * bulletspeed;
+                bulletFired.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
 
                 /*// Add more or less force to rotation according to the angle between Up Vector and shot's direction
                 if (Vector2.Angle(Vector2.up, direction) > 170)
@@ -113,52 +112,75 @@ public class PlayerController : MonoBehaviour
                 else if (Vector2.Angle(Vector2.up, direction) <= 170 && Vector2.Angle(Vector2.up, direction) > 165)
                     rb.AddTorque(-rb.angularVelocity + midanglerotforce);
                 else if (Vector2.Angle(Vector2.up, direction) < 165)
-                    rb.AddTorque(-rb.angularVelocity + faranglerotforce);
+                    rb.AddTorque(-rb.angularVelocity + faranglerotforce);*/
 
                 // Screen Shake
-                // Camera.main.transform.DOShakePosition(duration, new Vector3(0, strenght, 0), 20, 90);*/
+                Camera.main.transform.position = new Vector3(0, 0, -10);
+                Camera.main.transform.DOShakePosition(duration, new Vector3(strenght/2,strenght,0), 20, 90);
+                bulletNumber--;
             }
         }
-#endif
+        #endif
+
 
         // INPUTS FOR ANDROID
 #if UNITY_ANDROID
         for (int i = 0; i < Input.touchCount; ++i)
         {
-            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            if (Input.GetTouch(0).phase == TouchPhase.Began && Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y < transform.position.y)
             {
-                Vector2 direction = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position) - this.transform.position;
+                Vector2 direction = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position) - this.transform.position;
                 direction.Normalize();
 
                 if (gm.gamestarted == false)
                 {
                     gm.gamestarted = true;
-                    rb.simulated = true;
-                    rb.velocity = Vector2.up * 5;
-                    rb.AddTorque(700);
                 }
                 else
                 {
-                    rb.velocity = direction * -3f;
+                    rb.AddForce(new Vector2(Mathf.Clamp(-direction.x * sidepunch,-maxsidepunch,maxsidepunch), -direction.y) * firepunch, ForceMode2D.Impulse);
                     bulletFired = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
 
                     // Rotate the bullet towards the input
                     float rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
                     bulletFired.transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
 
-                    // Add more or less force to rotation according to the angle between Up Vector and shot's direction
-                    if (Vector2.Angle(Vector2.up, direction) > 170)
-                        rb.AddTorque(-rb.angularVelocity + 50 * -1);
-                    else if (Vector2.Angle(Vector2.up, direction) <= 170 && Vector2.Angle(Vector2.up, direction) > 165)
-                        rb.AddTorque(-rb.angularVelocity + 250 * -1);
-                    else if (Vector2.Angle(Vector2.up, direction) < 165)
-                        rb.AddTorque(-rb.angularVelocity + 500 * -1);
+                    bulletFired.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
 
                     // Screen Shake
-                    Camera.main.transform.DOShakePosition(duration, new Vector3(0, strenght, 0), 20, 90);
+                    Camera.main.transform.position = new Vector3(0, 0, -10);
+                    Camera.main.transform.DOShakePosition(duration, new Vector3(strenght / 2, strenght, 0), 20, 90);
                 }
             }
         }
-    }
 #endif
+    }
+
+
+    void BackForceY()
+    {
+        distanceOffsetY = transform.position.y - targetPosition.y;
+        float distanceToMax = maxOffsetY - distanceOffsetY;
+
+        if (Mathf.Abs(distanceOffsetY) > 0.1f)
+        {
+            Vector2 vectorToNormalize = new Vector2(0f, targetPosition.y - transform.position.y);
+            vectorToNormalize.Normalize();
+            rb.AddForce(vectorToNormalize * backForceY * Mathf.Abs(distanceOffsetY * 1.2f / maxOffsetY), ForceMode2D.Force);
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, startOffset - maxOffsetY, startOffset + maxOffsetY + 1), transform.position.z);
+        }
+    }
+
+    void BackForceX()
+    {
+        distanceOffsetX = transform.position.x;
+
+        if (Mathf.Abs(distanceOffsetX) > 0.1f)
+        {
+            Vector2 vectorToNormalize = new Vector2(transform.position.x, 0f);
+            vectorToNormalize.Normalize();
+            rb.AddForce(vectorToNormalize * -backForceX * Mathf.Abs(distanceOffsetX * 1.2f / maxOffsetX), ForceMode2D.Force);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -maxOffsetX, maxOffsetX), transform.position.y, transform.position.z);
+        }
+    }
     } 
---
+++ Assets/_Script/Wall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    public List<Sprite> wallSprites = new List<Sprite>();
+    public bool left;
+    public bool right;
+
+	// Use this for initialization
+	void Start () {
+        if (left)
+            GetComponent<SpriteRenderer>().sprite = wallSprites[0];
+        if(right)
+            GetComponent<SpriteRenderer>().sprite = wallSprites[1];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
