a352e7a500d3ff0da9dbb5423b55515b4caa08e8
Yue <duyue36@gmail.com>
2017-03-02 16:27:18 +0100

    Update EnemyController(AI can choose random actions / smooth pointToPoint function)

---
+++ Assets/_Script/Enemy/LookAtTarget2D.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget2D : MonoBehaviour {
+
+
+    private GameObject target;
+    
+	void Start () {
+        target = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        LookAtTarget();
+	}
+
+    void LookAtTarget()
+    {
+        Vector3 diff = target.transform.position - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
+
+    void Helper()
+    {
+
+        Debug.DrawLine(transform.position, target.transform.position, Color.blue);
+    }
+}
