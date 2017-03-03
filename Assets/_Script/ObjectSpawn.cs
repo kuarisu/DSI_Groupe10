6e6d4da46f59713cb36e5fe7e92aed8f7a2590b2
ETU\s.debank <s.debank@rubika-edu.com>
2017-03-03 14:01:57 +0100

    Add Fixed Prefab
    Add Fixed Prefab for LD

---
+++ Assets/_Script/ObjectSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawn : MonoBehaviour
+{
+
+    public GameObject m_ObjectToBeSpawn;
+
+    private void OnEnable()
+    {
+        GameObject clone = Instantiate(m_ObjectToBeSpawn, transform.position, Quaternion.identity);
+        clone.transform.parent = this.gameObject.transform.root.transform;
+        this.gameObject.SetActive(false);
+    }
+}
