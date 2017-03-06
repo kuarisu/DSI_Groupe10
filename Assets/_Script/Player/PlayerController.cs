2f5d35629ea9b0d9607b12b703add10b05973c4b
ETU\n.pankowiak <n.pankowiak@rubika-edu.com>
2017-03-06 16:26:31 +0100

    Recoil system & feedback added when empty magazine

--- Assets/_Script/Player/PlayerController.cs
+++ Assets/_Script/Player/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
 
     public GameObject bullet;
+    public GameObject noBullet;
     GameObject bulletFired;
     bool isHoldingDown;
     bool doRotate;
@@ -67,6 +68,7 @@ public class PlayerController : MonoBehaviour
     public bool passedFirstChunkpoint;
     public bool isInChunkPoint;
     public Vector2 targetPosition;
+    float rot_z;
 
     // Use this for initialization
     void Awake()
@@ -111,7 +113,7 @@ public class PlayerController : MonoBehaviour
 
         //INPUTS FOR EDITOR AND STANDALONE
 #if UNITY_EDITOR || UNITY_STANDALONE
-        if (Input.GetMouseButton(0) && Camera.main.ScreenToWorldPoint(Input.mousePosition).y < transform.position.y && Time.time > rateOfFire + lastShot && currentBullet > 0)
+        if (Input.GetMouseButton(0) && Camera.main.ScreenToWorldPoint(Input.mousePosition).y < transform.position.y && Time.time > rateOfFire + lastShot)
         {
             Fire();
             lastShot = Time.time;
@@ -156,26 +158,35 @@ public class PlayerController : MonoBehaviour
         direction.Normalize();
 
         rb.AddForce(new Vector2(Mathf.Clamp(-direction.x * sidepunch, -maxsidepunch, maxsidepunch), -direction.y) * firepunch, ForceMode2D.Impulse);
-        bulletFired = Instantiate(bullet, transform.position, transform.rotation);
-        bulletFired.transform.SetParent(gm.levelManager.currentChunk[1].transform);
+        rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
+
+        if (currentBullet > 0)
+        {
+            bulletFired = Instantiate(bullet, transform.position, transform.rotation);
+            bulletFired.transform.SetParent(gm.levelManager.currentChunk[1].transform);
+
+            if (!isInChunkPoint)
+                Bullets(-1);
+
+            bulletFired.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+        }else
+        {
+            bulletFired = Instantiate(noBullet, transform.position, transform.rotation);
+            bulletFired.transform.SetParent(gm.levelManager.currentChunk[1].transform);
+        }
 
         // Rotate the bullet and player towards the input
-        float rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         bulletFired.transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
-        transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
         backforceTimer = 0f;
 
-        bulletFired.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
-
         // Screen Shake
-        if(gm.uiManager.isRight == true)
+        if (gm.uiManager.isRight == true)
             Camera.main.transform.DOMoveX(-Camera.main.orthographicSize * 0.035f, 2f);
         else if(gm.uiManager.isRight == false)
             Camera.main.transform.DOMoveX(Camera.main.orthographicSize * 0.035f, 2f);
         Camera.main.transform.DOShakePosition(duration, new Vector3(strenght / 2, strenght, 0), 20, 90);
-
-        if(!isInChunkPoint)
-            Bullets(-1);
 
         DOTween.Kill("Rotation");
         isRotating = false; 
